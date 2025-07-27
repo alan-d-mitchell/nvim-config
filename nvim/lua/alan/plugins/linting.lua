b3ec0fbbac6304a88aa65af2e0b98b6a539da6ca
Alan Mitchell <mitchellalan1999@icloud.com>
2025-07-27 16:14:12 -0500

    added linting

---
+++ nvim/lua/alan/plugins/linting.lua
@@ -0,0 +1,22 @@
+return {
+    "mfussenegger/nvim-lint",
+    event = {
+        "BufReadPre",
+        "BufNewFile"
+    },
+    config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+            rust = { "clippy" },
+            lua = { "luacheck" },
+            c = { "clangtidy" },
+        }
+
+        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+    end,
+}
