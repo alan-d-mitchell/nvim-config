17ecd508c531345b5884a8d8222b638b05253468
Alan Mitchell <mitchellalan1999@icloud.com>
2025-08-12 11:47:31 -0500

    quick sync

--- nvim/lua/alan/plugins/colorscheme.lua
+++ nvim/lua/alan/plugins/colorscheme.lua
@@ -1,26 +1,22 @@
+-- Kanagawa Paper --
+--return {
+--    "thesimonho/kanagawa-paper.nvim",
+--    lazy = false,
+--    priority = 1000,
+--    init = function()
+--        vim.cmd.colorscheme("kanagawa-paper")
+--    end,
+--    opts = {...},
+--}
+
+-- Vesper --
 return {
-    -- Kanagawa Paper --
-    -- "thesimonho/kanagawa-paper.nvim",
-    --lazy = false,
-    --priority = 1000,
-    --init = function()
-        --vim.cmd.colorscheme("kanagawa-paper")
-    --end,
-    --opts = {...},
-
-    -- Temple OS --
-    --"lunarvim/templeos.nvim",
-    --lazy = false,
-    --priority = 1000,
-    --init = function()
-        --vim.cmd.colorscheme("templeos")
-    --end,
-
-    -- Vesper --
     "datsfilipe/vesper.nvim",
     lazy = false,
     priority = 1000,
     init = function()
         vim.cmd.colorscheme("vesper")
     end,
+    opts = { ... },
 }
+
