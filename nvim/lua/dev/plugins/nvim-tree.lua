0a619a77e5dd0a7c220a8ad70b520d02995a6fe8
Alan Mitchell <mitchellalan1999@icloud.com>
2025-08-30 08:47:57 -0500

    update: show files that are git ignored

--- nvim/lua/dev/plugins/nvim-tree.lua
+++ nvim/lua/dev/plugins/nvim-tree.lua
@@ -1,16 +1,39 @@
 return {
     "nvim-tree/nvim-tree.lua",
     dependencies = "nvim-tree/nvim-web-devicons",
-    config = function() 
+    config = function()
         -- recommended settings
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
 
-        local nvimtree = require("nvim-tree").setup()
+        require("nvim-tree").setup({
+            renderer = {
+                group_empty = true,
+                icons = {
+                    show = {
+                        file = true,
+                        folder = true,
+                        folder_arrow = true,
+                        git = true,
+                    },
+                },
+            },
+            filters = {
+                dotfiles = false,
+                custom = {},
+                exclude = {},
+            },
+            git = {
+                enable = true,
+                ignore = false,
+                timeout = 400,
+            },
+        })
 
         local keymap = vim.keymap
         keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>") -- toggle tree
         keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>") -- toggle tree for directory of current file
         keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>") -- reload
-    end
+    end,
 }
+
