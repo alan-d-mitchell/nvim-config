2c535830cd075549933aee9810a50da550745c24
Alan Mitchell <mitchellalan1999@icloud.com>
2025-10-06 09:47:17 -0500

    update: added psvm to java file auto command

--- nvim/lua/dev/core/autocmds.lua
+++ nvim/lua/dev/core/autocmds.lua
@@ -4,7 +4,9 @@ vim.api.nvim_create_autocmd("BufNewFile", {
         local filename = vim.fn.expand("%:t:r")
         local template = {
             "public class " .. filename .. " {",
-            "",
+            "\tpublic static void main(String[] args) {",
+            " ",
+            "\t}",
             "}"
         }
         vim.api.nvim_buf_set_lines(0, 0, -1, false, template)
